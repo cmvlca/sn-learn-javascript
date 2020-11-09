3eb12d2b187eb95772c8288d505168b7d4f371ad
cmvlca <74063607+cmvlca@users.noreply.github.com>
2020-11-09 10:36:50 -0600

    Update L10S01.js

--- Lesson_10/L10S01.js
+++ Lesson_10/L10S01.js
@@ -1,5 +1,5 @@
 //
-// L10S01 - Arithmetic Operators
+// L10S01 - Arithmetic Comparisons for true and false
 //
 var a = 0;
 var b = 1;
@@ -7,5 +7,6 @@ gs.info(a < b);
 
 var n = '3';
 var i = 3;
-gs.info(n == i); // REALLY?!!
+gs.info(n == i); // JavaScript is a loose language so if it is 'close enough' it will still run the script
+gs.info(n === i); // JavaScript can test equality of the value and the date type by using the === 
 gs.info(i = 4); // WATCH OUT!!!
