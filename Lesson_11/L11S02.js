704e5a700fd0500e119aa0cebf9a3042c5f806ba
cmvlca <74063607+cmvlca@users.noreply.github.com>
2020-11-09 11:15:27 -0600

    Update L11S02.js

--- Lesson_11/L11S02.js
+++ Lesson_11/L11S02.js
@@ -26,32 +26,60 @@
 //      |         |  false  |  true   |
 //      +---------+---------+---------+
 //
+//
+// And Expression that is true
+//
 var a = 1;
 var b = 3;
 var c = 5;
 
 if (a < b && b < c)
   gs.info('a b c are in order');
+//
+// And Expression that is false
+//
+var d = 11;
+var e = 3;
+var f = 5;
 
-if (b > a || b > c)
+if (d < e && e < f) {
+  gs.info('d e f are in order');
+} else {
+	gs.info('d e f are NOT in order!');
+}
+//
+// Or expression where one test is true
+//
+if (b > a || b > c) {
   gs.info('b is greater than one of them.');
-
+}
+//
+// Or expression where both tests are false
+//
+if (b < a || b > c) {
+  gs.info('b is greater than one of them.');
+} else {
+  gs.info('b is lower than a and c.');
+} 
+//
 // Note, indentation can be deceptive!!!
-if (a < b)
-  if (b < c) {
+//
+if (a < b) { // This can be an "and" test where this "if" has to pass first and can be used to decompose the statement for troubleshooting purposes
+  if (b < c) { // This can be an "and" test where the second "if" has to pass for the rest of the actions to take place
     gs.info('a b c are in order');
     gs.info(' that means a is less than c');
-  }
-else
+  } else { // This else statement belongs to the second "if" statement. If the first "if" statement does not pass, the second if/else statement will not run
   gs.info('a is greater than or equal to b');
-
-var valveOpen = true;
-if (valveOpen == true)
+  } 
+}
+// Variable for Valve Status
+var valveStatus = true;
+//
+if (valveStatus) { // Can shortcut from valveStatus == true to just if (valveStatus)
   gs.info('Valve is currently open');
-
-if (bool)
-  gs.info('Valve is currently open');
-
-var valveOpen = false;
-if (!valveOpen)
+}
+// Variable for Valve Status
+var valveStatus = false;
+if (!valveStatus) {
   gs.info('Valve is currently closed');
+}
