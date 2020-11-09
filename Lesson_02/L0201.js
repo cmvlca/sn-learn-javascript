c472cdd955b1ff7e7b632c62ac380fceb7c77220
cmvlca <74063607+cmvlca@users.noreply.github.com>
2020-11-08 19:09:41 -0600

    Update L0201.js

--- Lesson_02/L0201.js
+++ Lesson_02/L0201.js
@@ -3,32 +3,32 @@
 //
 
 // Required:
-var i = 0; i++        // <-- semicolon obligatory
-                      //     (but optional before newline)
-var i = 0             // <-- semicolon optional
-    i++               // <-- semicolon optional
+var i = 0; i++        // <-- semicolon obligatory to show where one line endes and another begins
+                      // 
+var i = 0             // <-- semicolon optional when a new line is created but not best practice
+    i++               // 
 
 // Optional:
-var i;                        // variable declaration
-i = 5;                        // value assignment
-i = i + 1;                    // value assignment
-i++;                          // same as above
-var x = 9;                    // declaration & assignment
-var fun = function() {...};   // var decl., assignmt, and func. defin.
-alert("hi");                  // function call
+var i;                        // variable declaration doesn't need a semicolon but best practice to add the semicolon
+i = 5;                        // value assignment doesn't need a semicolon but best practice to add the semicolon
+i = i + 1;                    // value assignment doesn't need a semicolon but best practice to add the semicolon
+i++;                          // same as above doesn't need a semicolon but best practice to add the semicolon
+var x = 9;                    // declaration & assignment doesn't need a semicolon but best practice to add the semicolon
+var fun = function() {...};   // var decl., assignmt, and func. defin. doesn't need a semicolon but best practice to add the semicolon
+alert("hi");                  // function call doesn't need a semicolon but best practice to add the semicolon
 
-// Avoid
+// Avoid semicolons for if/else, for, while
 // NO semicolons after }:
 if  (...) {...} else {...}
 for (...) {...}
 while (...) {...}
 
-// BUT:
+// BUT for do/while it is good to put a semicolon:
 do {...} while (...);
 
 // function statement: 
 function (arg) { /*do this*/ } // NO semicolon after }
 
-// Exception:
+// Exception exceptions for semi-colon:
 for (var i=0; i < 10; i++)  {/*actions*/}       // correct
 for (var i=0; i < 10; i++;) {/*actions*/}       // SyntaxError
