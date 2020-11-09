9ab0c9095e489de71e0c0fb5ac04c419c6527283
cmvlca <74063607+cmvlca@users.noreply.github.com>
2020-11-09 10:52:32 -0600

    Update L11S01.js

--- Lesson_11/L11S01.js
+++ Lesson_11/L11S01.js
@@ -5,23 +5,39 @@ var a = 1;
 var b = 3;
 var c = 5;
 if (a < b)
-  gs.info('a is less than b');
+  gs.info('a is less than b'); //only runs the next statement use Curly Brackets to create a block of code for if statements
 
-// else
-if (a < b)
+if (a < b) {
+  gs.info('a is less than b'); //Will run all Curly Brackets statements if true
+	gs.info('Hello world!');
+}
+
+//Boolean Variables on their own
+var bool = a < b;
+if (bool); {
+  gs.info('a is less than b'); //only runs the next statement use Curly Brackets to create a block of code for if statements
+	gs.info('Hello world!');
+}
+//
+// If-Else statement practice. Best practice to use Curly Brackets even for single lines
+//
+if (a < b) {
   gs.info('a is less than b');
-else
+} 
+else {
   gs.info('a is greater than or equal to b');
+}
 
-// Else if and else
-if (a < b)
+// Else if and else is about testing multiple outcomes
+if (a < b) {
   gs.info('a is less than b');
-else if (a > b)
+} else if (a > b) {
   gs.info('a is greater than b');
-else if (a == b)
+} else if (a == b) {
   gs.info('a equals b');
-else
+} else {
   gs.info('Uh-oh');
+}
 
 if (a < b)
   if (b < c)
