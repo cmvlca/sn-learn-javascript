e10ebd842eb3a4ed44ea5cfa34c690e51404bdb5
cmvlca <74063607+cmvlca@users.noreply.github.com>
2020-11-08 19:26:07 -0600

    Update L0301.js

--- Lesson_03/L0301.js
+++ Lesson_03/L0301.js
@@ -5,9 +5,9 @@ var name   = 'Chuck';   // Simple string variable
 var i      = 0;         // Simple integer variable
 var answer = true;      // Simple boolean variable
 
-// Naming
+// Naming is important to understand what the variable means and how it is being used further in the code
 var c = "http://www.amazon.com";  // Not ideal
-var case = 'CASE0010001';         // Reserved word!
+var caseNumber = 'CASE0010001';         // There are reserved words that can't be used as variables! 
 var lastEntryInTheListWithRelatedRecords = true; // a bit long?
 // personCount      // Indicates a counter/integer
 // personList       // Must be a list
